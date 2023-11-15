3b61dd7ee036957b6514dcbf281364797b4ae006
Rick <ricleyreis@gmail.com>
2023-11-15 18:36:53 -0300

    atualizando aula 10

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,31 @@ const tarefas = [
 
 export default function App() {
 
-  // const [tarefas, setTarefas] = useState([])
+  const [tarefas, setTarefas] = useState([])
 
-  // useEffect(()=> {
-  //   async function buscarDados(){
-  //     const resultado = await fetch('https://jsonplaceholder.typicode.com/todos')
-  //     const resultadoFinal = await resultado.json()
+  useEffect(()=> {
 
-  //     return resultadoFinal
-  //   }
+    async function buscarDados(){
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(res => setTarefas(res))
+    }
 
-  //   buscarDados().then(res => setTarefas(res))
-  // }, [])
+    buscarDados()
+  }, [])
 
   return (
     <div>
-      <MeuContador/>
+      <h1>Deploy na vercel</h1>
+      <div>
+        {tarefas.map((tarefa) => {
+          return(
+            <div className="task-item" key={tarefa.id}>
+              <p> {tarefa.id} - {tarefa.title}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
